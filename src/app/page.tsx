be1ecf9b84a15f9b7f5698321b77ac19a4936c9e
Austin <aconnor731@gmail.com>
2024-08-03 15:32:01 -0400

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,50 @@
-'use client'
-import { useState } from 'react';
-import { TextField, Button, Container, Typography } from '@mui/material';
+import Link from "next/link";
+import React from "react";
+import Particles from "@/app/_components/particles";
 
-export default function t() {
-  const [to, setTo] = useState('');
-  const [subject, setSubject] = useState('');
-  const [text, setText] = useState('');
-  const [html, setHtml] = useState('');
+const navigation = [
+  { name: "Blog", href: "/posts" },
+];
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const response = await fetch('/api/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ to, subject, text, html }),
-    });
-
-    const data = await response.json();
-    alert(data.message);
-  };
-
+export default function Home() {
   return (
-    <Container className="flex flex-col items-center justify-center min-h-screen py-8 bg-gradient-to-b from-gray-200 to-gray-300">
-      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
-        <Typography variant="h4" component="h1" gutterBottom className="text-2xl font-bold mb-6">
-          Send Email
-        </Typography>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <TextField
-            label="To"
-            type="email"
-            fullWidth
-            value={to}
-            onChange={(e) => setTo(e.target.value)}
-            required
-          />
-          <TextField
-            label="Subject"
-            type="text"
-            fullWidth
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            required
-          />
-          <TextField
-            label="Text"
-            multiline
-            rows={4}
-            fullWidth
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            required
-          />
-          {/* <TextField
-            label="HTML"
-            multiline
-            rows={4}
-            fullWidth
-            value={html}
-            onChange={(e) => setHtml(e.target.value)}
-          /> */}
-          <Button type="submit" fullWidth variant="contained" color="primary">
-            Send Email
-          </Button>
-        </form>
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="my-16 animate-fade-in">
+        <ul className="flex items-center justify-center gap-4">
+          {navigation.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </ul>
+      </nav>
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        austin
+      </h1>
+
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="my-16 text-center animate-fade-in">
+        <h2 className="text-sm text-zinc-500 ">
+          I'm building{" "}
+          <Link
+            target="_blank"
+            href="https://unkey.dev"
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            unkey.dev
+          </Link> to solve API authentication and authorization for developers.
+        </h2>
       </div>
-    </Container>
+    </div>
   );
+
 }
---
+++ src/app/playground/openai/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import GPTComponent from '@/app/ui/GPTComponent';
+import * as React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Open Ai Playground</h2>
+      <GPTComponent />
+    </div>
+  );
+}
